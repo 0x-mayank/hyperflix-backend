c404284f6c40f5c58f2ef997420f45e9fd05b55b
0x-mayank <mayankm1946@gmail.com>
2026-06-03 19:01:19 +0530

    fix

--- src/routes/animeRoutes.js
+++ src/routes/animeRoutes.js
@@ -4,7 +4,6 @@ const anime = require("../controllers/animeController");
 router.get("/trending", anime.getTrending);
 router.get("/popular", anime.getPopular);
 router.get("/top-rated", anime.getTopRated);
-router.get("/:id/stream", anime.getStream);
 router.get("/:id", anime.getDetails);
 
 module.exports = router;
