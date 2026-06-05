c67328142d8885c5c6ddcb2e06a39609cc22cac9
0x-mayank <mayankm1946@gmail.com>
2026-06-05 14:50:59 +0530

    changes

--- src/services/nhdService.js
+++ src/services/nhdService.js
@@ -1,7 +1,9 @@
 const PLAYER_OPTIONS =
   "?title=true" +
   "&nextbutton=true"+
-  "&player=jw";
+  "&player=jw"+
+  "&secondaryColor=170000"+
+  "&primaryColor=B20710";
 
 exports.movieUrl = (tmdbId) => {
   return `https://vidlink.pro/movie/${tmdbId}${PLAYER_OPTIONS}`;
